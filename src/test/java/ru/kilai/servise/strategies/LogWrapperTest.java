e1f3e32044da31cdf93a290063a4122695877963
Mikhail <kilai.ru@yandex.ru>
2022-09-10 00:51:40 +0900

    Добавлена возможность составлять Action в цепочки.

---
+++ src/test/java/ru/kilai/servise/strategies/LogWrapperTest.java
@@ -0,0 +1,27 @@
+package ru.kilai.servise.strategies;
+
+import io.netty.handler.codec.http.multipart.HttpData;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class LogWrapperTest {
+
+    @Test
+    void apply() throws IOException {
+        var parameters = spy(new GetRequestParameters());
+        var httpData = mock(HttpData.class);
+        when(httpData.get()).thenReturn("".getBytes());
+        when(parameters.apply(httpData).log()).thenReturn(Flux.just("ans"));
+
+        var stringFlux = new LogWrapper<>(parameters).apply(httpData);
+
+        assertEquals("ans", stringFlux.blockFirst());
+
+        verify(parameters, times(1)).apply(httpData);
+    }
+}
---
+++ src/test/java/ru/kilai/servise/strategies/SchedulerWrapperTest.java
@@ -0,0 +1,33 @@
+package ru.kilai.servise.strategies;
+
+import io.netty.handler.codec.http.multipart.HttpData;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import ru.kilai.config.SimplerThreadFactory;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SchedulerWrapperTest {
+
+    @Test
+    void apply() throws IOException {
+        var parameters = spy(new GetRequestParameters());
+        var httpData = mock(HttpData.class);
+        var executorService = Executors.newFixedThreadPool(4,
+                new SimplerThreadFactory("worker-"));
+        when(httpData.get()).thenReturn("".getBytes());
+        when(parameters.apply(httpData).publishOn(Schedulers.fromExecutor(executorService)))
+                .thenReturn(Flux.just("ans"));
+
+        var stringFlux = new SchedulerWrapper<>(parameters, executorService).apply(httpData);
+
+        assertEquals("ans", stringFlux.blockFirst());
+
+        verify(parameters, times(1)).apply(httpData);
+    }
+}
